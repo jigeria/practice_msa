ebea61bb7eeee06447d65bc113ab742c20e1e852
parksm114 <jigeria@naver.com>
2023-03-04 21:53:22 +0900

    hotfix

--- card/card.py
+++ card/card.py
@@ -12,21 +12,22 @@ api = Api(app)
 USER_NAME = "sangmin"
 TABLE_NAME = "CARD_INFO"
 
+
 @api.route("/card")
 class ReadCardDb(Resource):
     def get(self):
         db = Databases(host="db", port=5432)
-        
+
         sql = f"SELECT * from {TABLE_NAME} WHERE user_name='{USER_NAME}';"
         db.cursor.execute(sql)
         result = db.cursor.fetchall()
-        
+
         print("result: ", result)
 
         return {
             "user_id": json.dumps(result[0][0]),
             "user_name": json.dumps(result[0][1]),
-            "card_number": json.dumps(result[0][2])
+            "card_number": json.dumps(result[0][2]),
         }
 
 
--- user/user.py
+++ user/user.py
@@ -11,30 +11,31 @@ api = Api(app)
 USER_NAME = "sangmin"
 TABLE_NAME = "USER_ACOOUNT"
 
+
 @api.route("/user")
 class ReadUserDb(Resource):
     def get(self):
         db = Databases(host="db", port=5432)
-        
+
         sql = f"SELECT * from {TABLE_NAME} WHERE user_name='{USER_NAME}';"
         db.cursor.execute(sql)
         result = db.cursor.fetchall()
-        
+
         print("result: ", result)
 
         return {
             "user_id": json.dumps(result[0][0]),
             "user_name": json.dumps(result[0][1]),
-            "phone_number": json.dumps(result[0][2])
+            "phone_number": json.dumps(result[0][2]),
         }
 
 
 if __name__ == "__main__":
     # db = Databases(port=5555)
-    
+
     # sql = f"SELECT * from USER_ACOOUNT WHERE user_name='sangmin';"
     # db.cursor.execute(sql)
     # result = db.cursor.fetchall()
-    
+
     # print("result: ", result)
     app.run(debug=True, host="0.0.0.0", port=5100)
